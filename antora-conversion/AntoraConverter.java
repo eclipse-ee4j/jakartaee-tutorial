979a64afdc5a05110d54e9f9368b9f5205774104
Kito D. Mann <kito.mann@virtua.tech>
2023-05-29 22:01:57 -0400

    JETUT-56 Updated script and docs for processing Jakarta EE 9 versions of docs.

--- antora-conversion/AntoraConverter.java
+++ antora-conversion/AntoraConverter.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,10 +14,11 @@ import java.util.function.Consumer;
 
 class AntoraConverter {
 
-    record PageLocation(String path, String page) {
+    record PageLocation(String path, String page, String linkText) {
         public String getParentPage() {
             return path + ".adoc";
         }
+
         public String toString() {
             return path + "/" + getParentPage();
         }
@@ -26,14 +28,14 @@ class AntoraConverter {
 
     public static String replaceBetween(String str, String start, String end, String prefix, String fromString, String toString,
                                         BiFunction<String, String, String> contentUpdater) {
-        StringBuilder result = new StringBuilder();
+        final var result = new StringBuilder();
         int index = 0;
         int startIndex, endIndex;
 
         while ((startIndex = str.indexOf(start, index)) != -1 && (endIndex = str.indexOf(end, startIndex + start.length())) != -1) {
             result.append(str, index, startIndex + start.length());
-            String substring = str.substring(startIndex + start.length(), endIndex);
-            String updatedSubstring = substring.replace(fromString, toString);
+            var substring = str.substring(startIndex + start.length(), endIndex);
+            var updatedSubstring = substring.replace(fromString, toString);
 
             // don't add the prefix if nothing has changed
             if (!substring.equals(updatedSubstring)) {
@@ -54,14 +56,57 @@ class AntoraConverter {
         return result.toString();
     }
 
-    public static void processInbetween(String str, String start, String end, Consumer<String> updatedContentConsumer) {
+    public static String replaceInlineReferences(File file, String fileContent) {
+        StringBuilder result = new StringBuilder();
         int index = 0;
+        int startIndex, endIndex;
+        final String start = "<<";
+        final String end = ">>";
+
+        while ((startIndex = fileContent.indexOf(start, index)) != -1 && (endIndex = fileContent.indexOf(end, startIndex + start.length())) != -1) {
+            result.append(fileContent, index, startIndex);
+            final var rawReference = fileContent.substring(startIndex, endIndex + end.length());
+            var referenceBody = rawReference.substring(start.length(), rawReference.length() - end.length());
+            var updatedReferenceBody = createAnchor(referenceBody);
+            String updatedReference;
+
+            var pageLocation = anchorMap.get("#" + updatedReferenceBody) ;
+            pageLocation = pageLocation != null ? pageLocation : anchorMap.get("#" + referenceBody);
+            if (pageLocation == null) {
+                System.out.printf("  => WARNING: No page location found for anchor \"%s\" or \"%s\"; the file with the anchor may not have been within the startPath%n",
+                        updatedReferenceBody, referenceBody);
+                updatedReference = rawReference;
+            } else {
+                if (isInlineReference(file, referenceBody, pageLocation)) {
+                    updatedReference = start + updatedReferenceBody +  end;
+                } else {
+
+                    updatedReference = String.format("xref:%s/%s#%s[%s]", pageLocation.getParentPage(),
+                            pageLocation.getParentPage(), updatedReferenceBody, pageLocation.linkText());
+                }
+            }
+            System.out.printf(" => Replacing inline reference \"%s\" with \"%s\"%n", rawReference, updatedReference);
+            result.append(updatedReference);
+            index = endIndex + end.length();
+        }
+        result.append(fileContent, index, fileContent.length());
+        return result.toString();
+    }
+
+    public static void processInbetween(String str, Integer iterations, String start, String end, Consumer<String> updatedContentConsumer) {
+        int index = 0;
+        int count = 0;
         int startIndex, endIndex;
 
         while ((startIndex = str.indexOf(start, index)) != -1 && (endIndex = str.indexOf(end, startIndex + start.length())) != -1) {
             String substring = str.substring(startIndex + start.length(), endIndex);
             updatedContentConsumer.accept(substring);
             index = endIndex;
+            if (iterations == null || count < iterations) {
+                count++;
+            } else {
+                break;
+            }
         }
     }
 
@@ -69,7 +114,7 @@ class AntoraConverter {
         System.out.println("\uD83E\uDD16 Antora Converter: Convert raw Asciidoc files to Antora-compatible format");
         System.out.println("Walks through all folders and subfolders from the specified startPath and converts all .adoc files");
 
-        var files = new ArrayList<File>();
+        final var files = new ArrayList<File>();
         if (args.length == 0) {
             System.out.println("Usage: AntoraConverter.java [startPath] dryRun?");
             System.out.println("       Where [startPath] is the root folder to search (usually a specific Antora module or component folder)");
@@ -97,14 +142,43 @@ class AntoraConverter {
             System.out.println("\uD83D\uDD0E Pre-scanning file " + file.getAbsolutePath());
             String content = new String(Files.readAllBytes(file.toPath()));
             System.out.println("=> Processing anchor names");
-            processInbetween(content, "[[", "]]", (anchor) -> {
-                // NOTE: This currently will only work for files within the same module. This will need
-                // to be updated for files in different modules.
-                final var pageLocation = new PageLocation(file.getParentFile().getName(), file.getName());
-                anchorMap.put("#" + anchor, pageLocation);
-                System.out.printf("  => Stored anchor \"%s\" for %s%n", anchor, pageLocation);
-            });
+
+            processSectionHeadings(file, content);
+            processAnchors(file, content);
         }
+    }
+
+    private static void processSectionHeadings(File file, String content) {
+        // Start with the first heading (level 1 and 2)
+        processInbetween(content, 1, "= ", System.lineSeparator(), processSectionAnchor(file));
+        processInbetween(content, 1, "== ", System.lineSeparator(), processSectionAnchor(file));
+        // Then process all lower section headings
+        for (int i = 3; i < 7; i++) {
+            final var prefix = System.lineSeparator() + "=".repeat(i) + " ";
+            processInbetween(content, null, prefix, System.lineSeparator(), processSectionAnchor(file));
+        }
+    }
+
+    private static void processAnchors(File file, String content) {
+        processInbetween(content, null, "[[", "]]", (anchor) -> {
+            final var pageLocation = new PageLocation(file.getParentFile().getName(), file.getName(), null);
+            anchorMap.put("#" + anchor, pageLocation);
+            System.out.printf("  => Stored anchor \"%s\" for %s%n", anchor, pageLocation);
+        });
+    }
+
+    private static Consumer<String> processSectionAnchor(File file) {
+        return (heading) -> {
+            final String anchor = createAnchor(heading);
+            final var pageLocation = new PageLocation(file.getParentFile().getName(), file.getName(), heading);
+            anchorMap.put("#" + anchor, pageLocation);
+            System.out.printf("  => Stored anchor \"%s\" for section \"%s\" for %s%n", anchor, heading, pageLocation);
+        };
+    }
+
+    private static String createAnchor(String str) {
+        final var strWithPrefix = str.charAt(0) == '_' ? str : "_" + str;
+        return strWithPrefix.toLowerCase().replaceAll("[\\s-]", "_").replace(":", "").replaceAll("[?,&()]", "");
     }
 
     private static void process(ArrayList<File> files, boolean dryRun) throws IOException {
@@ -115,8 +189,9 @@ class AntoraConverter {
             int changeCount = 0;
 
             if (content.contains(("<<"))) {
-                System.out.println("=> Updating in-page anchor references");
-                content = replaceBetween(content, "<<", ">>", "_", "-", "_", null);
+                System.out.println("=> Updating in-page anchor links");
+//                content = replaceBetween(content, "<<", ">>", "_", "-", "_", null);
+                content = replaceInlineReferences(file, content);
                 if (!dryRun) {
                     Files.write(file.toPath(), content.getBytes());
                 }
@@ -143,9 +218,7 @@ class AntoraConverter {
                         return newLink;
                     }
 
-                    // If this page name contains the parent's page name, it's either the parent page itself or an included page,
-                    // so it's an internal reference.
-                    if (file.getName().contains(pageLocation.getParentPage())) {
+                    if (isInlineReference(file, newLink, pageLocation)) {
                         return newLink;
                     }
                     final var updatedLink = pageLocation + newLink;
@@ -165,9 +238,17 @@ class AntoraConverter {
                 }
                 changeCount++;
             }
-            if (content.contains("image:") && !content.contains("image:common:")) {
+            if (content.contains("xrefstyle=full")) {
+                System.out.println("=> Removing xrefstyle=full (it is now a global attribute)");
+                content = content.replace("xrefstyle=full", "");
+                if (!dryRun) {
+                    Files.write(file.toPath(), content.getBytes());
+                }
+                changeCount++;
+            }
+            if (content.contains("image::") && !content.contains("image::common:")) {
                 System.out.println("=> Converting inline image references to block image references and adding module prefix");
-                content = content.replace("image:", "image::common:");
+                content = content.replace("image::", "image::common:");
                 if (!dryRun) {
                     Files.write(file.toPath(), content.getBytes());
                 }
@@ -187,5 +268,13 @@ class AntoraConverter {
                 System.out.println("=> " + changeCount + " changes made");
             }
         }
+    }
+
+    /**
+     * If this page name contains the parent's page name, it's either the parent page itself or an included page,
+     * so it's an internal reference.
+     **/
+    private static boolean isInlineReference(File file, String newLink, PageLocation pageLocation) {
+        return file.getName().contains(pageLocation.getParentPage().split(".adoc")[0]);
     }
 }
